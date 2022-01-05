9b161d26c6c9c376ff67b4ca03df28914923106f
Chris Hill <chrishill399@gmail.com>
2022-01-05 10:35:21 +0000

    Add check for undefined env

--- src/utils.js
+++ src/utils.js
@@ -7,10 +7,12 @@ const EST_GAS = 550000;
 exports.validateInput = (input) => {
   const errors = [];
 
-  if (input[0].length !== 64) errors.push("Invalid private key provided");
-  if (input[1].length !== 66) errors.push("Invalid TX Hash provided");
-  if (!(input[2] !== "mainnet" || input[2] !== "testnet"))
+  if (!input[0] || input[0] && input[0].length !== 64) errors.push("Invalid private key provided");
+  if (!input[1] || input[1].length !== 66) errors.push("Invalid TX Hash provided");
+  if (!input[2] || !(input[2] !== "mainnet" || input[2] !== "testnet"))
     errors.push("Invalid network provided");
+
+  if (errors.length === 3) errors.push("No user variables found. Check .env setup.");
 
   return errors;
 };
